825695ece1fad6714fd897b1e5351fed3eb0eabb
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2025-06-30 15:06:12 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,21 @@
+function Header (){
+
+    return(
+        <header>
+            <h1>my website</h1>
+            <nav>
+                <ul>
+                    <l1><a href="#">Home</a></l1>
+                    <l2><a href="#">About</a></l2>
+                    <l3><a href="#">Services</a></l3>
+                    <l4><a href="#">Contact</a></l4>
+
+                </ul>
+                <hr></hr>
+            </nav>
+        </header>
+
+    );
+
+}
+export default Header
