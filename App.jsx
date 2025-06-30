373cc9da877196dbc9ef1c9912ec9920522c774b
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2025-06-30 15:48:10 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,9 @@
-import Header from "./Header.jsx" 
-import Footer from "./Footer.jsx"
-import Food from "./assets/Food.jsx";
+import card from "/.card.jsx"
 function App() {
 
     return(
         <>
-                <Header/>
-                <Food/>
-                <Footer/>
+            <card/>
 
         </>
     );
---
+++ card.jsx
@@ -0,0 +1,14 @@
+function card(){
+    return(
+        <div> className="card"
+            <h2>
+                DEVYANSH BATRA
+            </h2>
+            <p>
+                I am an innovator
+            </p>
+
+        </div>
+    );
+}
+export default card
