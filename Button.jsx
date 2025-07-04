d5fd38ce76ea93c8a0d391ae59a9991dcccc54b0
DEVYANSH BATRA <121395496+devyanshbatra@users.noreply.github.com>
2025-07-05 00:12:23 +0530

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,13 @@
+function Button(){
+
+    const styles = {
+        backgroundcolor: "aqua",
+        color: "white",
+        padding: "10px 20px",
+        borderradius: "10px",
+        cursor: "pointer",
+
+    }
+    return( <button className='button'>Click Me</button>);
+}
+export default Button
